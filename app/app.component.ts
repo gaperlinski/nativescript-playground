d377d0b8ac16cfaa7c8bb416c94037fffa58d4c1
gaperlinski <apiejh@gmail.com>
2016-06-23 22:28:12 +0200

    initial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,20 @@
 import {Component} from "@angular/core";
+import {RouteConfig} from "@angular/router-deprecated";
+import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
+import {HTTP_PROVIDERS} from "@angular/http";
+import {LoginPage} from "./pages/login/login.component";
+import {ListPage} from "./pages/list/list.component";
 
 @Component({
-  selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  selector: "main",
+  directives: [NS_ROUTER_DIRECTIVES],
+  providers: [HTTP_PROVIDERS, NS_ROUTER_PROVIDERS],
+  template: "<page-router-outlet></page-router-outlet>"
 })
+
+@RouteConfig([
+  { path: "/Login", component: LoginPage, name: "Login", useAsDefault: true },
+  { path: "/List", component: ListPage, name: "List" }
+])
+
 export class AppComponent {}
---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,60 @@
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {User} from "../../shared/user/user";
+import {UserService} from "../../shared/user/user.service";
+import {Router} from "@angular/router-deprecated";
+import {Page} from "ui/page";
+import {Color} from "color";
+import {View} from "ui/core/view";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
+})
+
+export class LoginPage implements OnInit {
+  user: User;
+  isLoggingIn = true;
+  @ViewChild("container") container: ElementRef;
+
+  constructor(private _router: Router, private _userService: UserService, private page: Page) {
+	  this.user = new User();
+	}
+	ngOnInit() {
+	  this.page.actionBarHidden = true;
+	  this.page.backgroundImage = this.page.ios ? "res://bg_login.jpg" : "res://bg_login";
+	}
+  submit() {
+	  if (this.isLoggingIn) {
+	    this.login();
+	  } else {
+	    this.signUp();
+	  }
+	}
+	login() {
+  this._userService.login(this.user)
+    .subscribe(
+      () => this._router.navigate(["List"]),
+      (error) => alert("Unfortunately we could not find your account.")
+    );
+	}
+  signUp() {
+	  this._userService.register(this.user)
+	    .subscribe(
+	      () => {
+	        alert("Your account was successfully created.");
+	        this.toggleDisplay();
+	      },
+	      () => alert("Unfortunately we were unable to create your account.")
+	    );
+	}
+  toggleDisplay() {
+	  this.isLoggingIn = !this.isLoggingIn;
+	  let container = <View>this.container.nativeElement;
+	  container.animate({
+	    backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+	    duration: 200
+	  });
+	}
+}
